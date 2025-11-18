e654b5fec4d7e7a9d42cea1844e6de10b725c940
lavoplusadm <lavoplus.adm@gmail.com>
2025-11-17 23:55:45 -0300

    se soluciona problema en la imagen de Open Graph sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -13,51 +13,15 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: `${baseUrl}/#inicio`,
-      lastModified: currentDate,
-      changeFrequency: 'weekly',
-      priority: 1,
-    },
-    {
-      url: `${baseUrl}/#servicios`,
-      lastModified: currentDate,
-      changeFrequency: 'monthly',
-      priority: 0.9,
-    },
-    {
-      url: `${baseUrl}/#delivery`,
-      lastModified: currentDate,
-      changeFrequency: 'monthly',
-      priority: 0.85,
-    },
-    {
-      url: `${baseUrl}/#cobertura`,
-      lastModified: currentDate,
-      changeFrequency: 'monthly',
-      priority: 0.85,
-    },
-    {
-      url: `${baseUrl}/#convenios-empresariales`,
+      url: `${baseUrl}/nosotros`,
       lastModified: currentDate,
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: `${baseUrl}/#calidad`,
-      lastModified: currentDate,
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/#testimonios`,
+      url: `${baseUrl}/precios`,
       lastModified: currentDate,
       changeFrequency: 'weekly',
-      priority: 0.75,
-    },
-    {
-      url: `${baseUrl}/#contacto`,
-      lastModified: currentDate,
-      changeFrequency: 'monthly',
       priority: 0.9,
     },
   ]
